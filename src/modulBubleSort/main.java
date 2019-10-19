dfaa7cf8ca6a1d1f21811a623a4bb43eb42db4db
ASUS RYZEN <ASUS RYZEN@LAPTOP-9PMMSI13>
2019-10-19 11:42:31 +0700

    main

---
+++ src/modulBubleSort/main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modulBubleSort;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS RYZEN
+ */
+public class main {
+
+    public static void main(String[] args) {
+        String a[];
+        Scanner in = new Scanner(System.in);
+        System.out.print("input long array: ");
+        int longIndexArray = in.nextInt();
+        a = new String[longIndexArray];
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("input string" + i);
+            a[i] = in.next();
+        }
+        BubleSort.BubleSorting(a);
+    }
+}
